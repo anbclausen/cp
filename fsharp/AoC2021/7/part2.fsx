a8b5007dcca217639e05c6bfb6180e3a10c58896
Anders B. Clausen <anbclausen@gmail.com>
2021-12-07 17:45:58 +0100

    Advent of Code day 7

---
+++ fsharp/AoC2021/7/part2.fsx
@@ -0,0 +1,15 @@
+let pos =
+  System.IO.File.ReadLines "fsharp/AoC2021/7/input.txt" |> 
+  Seq.head |>
+  (fun s -> s.Split ",") |>
+  List.ofSeq |>
+  List.map int
+
+let minPos = List.min pos 
+let maxPos = List.max pos
+
+[for i in[minPos .. maxPos] do 
+  yield List.map (fun p -> List.fold (+) 0 [1..abs (p - i)]) pos] |>
+List.map List.sum |>
+List.min |>
+printfn "%i"
