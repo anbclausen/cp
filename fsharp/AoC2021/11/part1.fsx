c6557cdb4dbfd7cb4c8a2d49231188764016c443
Anders B. Clausen <anbclausen@gmail.com>
2021-12-12 19:35:47 +0100

    Advent of Code day 11

---
+++ fsharp/AoC2021/11/part1.fsx
@@ -0,0 +1,48 @@
+open System.Collections
+
+let chr_to_num c = 
+  c - 48
+
+let m = 
+  System.IO.File.ReadLines "fsharp/AoC2021/11/input.txt" |> 
+  Seq.map (Seq.map (int >> chr_to_num)) |>
+  Seq.map Array.ofSeq |>
+  Array.ofSeq
+
+let oob (x, y) = 
+  x < 0 || x >= m.[0].Length || y < 0 || y >= m.Length
+
+let m_indexes = 
+  [for i in [0..m.[0].Length - 1] do 
+    for j in [0..m.Length - 1] do 
+      yield (i, j)]
+
+(* some sort of dfs *)
+let rec flash (q: Queue) (x, y) = 
+  if m.[y].[x] > 9 && not (q.Contains (x, y)) then 
+    q.Enqueue((x, y))
+    let neighbors = 
+      [(x - 1, y);
+      (x + 1, y);
+      (x, y - 1);
+      (x, y + 1);
+      (x - 1, y - 1);
+      (x + 1, y - 1);
+      (x - 1, y + 1);
+      (x + 1, y + 1);]
+    for n in neighbors do 
+      if not (oob n) then 
+        let (nx, ny) = n 
+        m.[ny].[nx] <- m.[ny].[nx] + 1
+        flash q n
+
+let step () =
+  m_indexes |> List.iter (fun (x, y) -> m.[y].[x] <- m.[y].[x] + 1)
+  let flashed = new Queue()
+  m_indexes |> List.iter (flash flashed)
+  m_indexes |> List.iter (fun (x, y) -> if m.[y].[x] > 9 then m.[y].[x] <- 0)
+  flashed.Count
+
+[for _ in [1..100] do yield step ()] |>
+List.sum |>
+printfn "%i"
